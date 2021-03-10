1ce0f08e0456286a3c132a5f2a456ea84ca5a028
wardetu <tobeornottobeisgrammaticallyco@gmail.com>
2021-03-10 20:47:56 +0800

    initial changes

---
+++ COMPONENT/src/qp/operators/GraceHashJoin.java
@@ -0,0 +1,10 @@
+/*
+    Grace Hash Join algorithm
+ */
+package qp.operators;
+
+public class GraceHashJoin extends Join{
+    public GraceHashJoin(Operator left, Operator right, int type) {
+        super(left, right, type);
+    }
+}
