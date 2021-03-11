317f9912df8901e0ae68bff29945605fe6f1b05b
hai <1503.hai.nguyen@gmail.com>
2021-03-11 18:45:17 +0800

    add error handling if page size is to small in JOIN

--- hai-testcases/smallpagetests.sh
+++ hai-testcases/smallpagetests.sh
@@ -2,4 +2,5 @@ if [ ! -d "./testoutput" ]
 then
     mkdir testoutput
 fi
-echo "95\n1\n"| java QueryMain queries/query1.sql testoutput/query1.out # test with small page
+echo "95\n1\n"| java QueryMain queries/query1.sql testoutput/query1_smallpage.out # test with small page
+echo "160\n10\n1\n"| java QueryMain queries/query3.sql testoutput/query3_smallpage.out # test with small page
