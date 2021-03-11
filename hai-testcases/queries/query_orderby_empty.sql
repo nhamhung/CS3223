c6ea78f6cff4b34b5ce804476bdc791fc10b1cab
hai <1503.hai.nguyen@gmail.com>
2021-03-11 15:32:40 +0800

    add sort empty relation

---
+++ hai-testcases/queries/query_orderby_empty.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM Customer
+WHERE Customer.gender="3"
+ORDERBY Customer.gender,Customer.cid DESC
--- hai-testcases/sortbytests.sh
+++ hai-testcases/sortbytests.sh
@@ -6,3 +6,4 @@ fi
 echo "400\n10\n1\n"| java QueryMain queries/query_orderby.sql testoutput/out_orderby.out
 echo "400\n5\n1\n"| java QueryMain queries/query_orderby2.sql testoutput/out_orderby2.out
 echo "400\n1\n"| java QueryMain queries/query_orderby3.sql testoutput/out_orderby3.out
+echo "400\n1\n"| java QueryMain queries/query_orderby_empty.sql testoutput/out_orderby_empty.out
