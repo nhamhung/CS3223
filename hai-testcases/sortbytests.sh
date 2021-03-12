150c0a2a2cf8c27fec523e8fa9eca37973e9ef81
hai <1503.hai.nguyen@gmail.com>
2021-03-12 21:05:12 +0800

    add query for number of buffers in all cases

--- hai-testcases/sortbytests.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -d "./testoutput" ]
-then
-    mkdir testoutput
-fi
-
-#echo "400\n10\n1\n"| java QueryMain queries/query_orderby.sql testoutput/out_orderby.out
-echo "400\n5\n1\n"| java QueryMain queries/query_orderby2.sql testoutput/out_orderby2.out
-#echo "400\n1\n"| java QueryMain queries/query_orderby3.sql testoutput/out_orderby3.out
-#echo "400\n1\n"| java QueryMain queries/query_orderby_empty.sql testoutput/out_orderby_empty.out
