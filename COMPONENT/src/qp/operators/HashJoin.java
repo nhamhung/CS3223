2384ebd10095747183513e5bdbd6a4729dbde88c
wardetu <tobeornottobeisgrammaticallyco@gmail.com>
2021-03-17 03:53:34 +0800

    add hash-join

--- COMPONENT/src/qp/operators/HashJoin.java
+++ COMPONENT/src/qp/operators/HashJoin.java
@@ -3,8 +3,12 @@
  */
 package qp.operators;
 
+import com.skjegstad.utils.*;
+import org.apache.hive.common.util.*;
+
 public class GraceHashJoin extends Join{
     public GraceHashJoin(Operator left, Operator right, int type) {
         super(left, right, type);
     }
+
 }
