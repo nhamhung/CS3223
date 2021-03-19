706ec5a857c49f39a25152d221e215c7f72cd36c
hai <1503.hai.nguyen@gmail.com>
2021-03-19 17:48:29 +0800

    reset tables in hai-testcases

--- hai-testcases/setupTests.sh
+++ hai-testcases/setupTests.sh
@@ -1,9 +1,9 @@
 cd tables || { echo "Cannot cd into tables/"; exit 1; }
-#java RandomDB Customer 500
-#java RandomDB CartDetails 1000
-#java RandomDB Cart 500
-#java RandomDB EmptyCustomer 0
-#java RandomDB EmptyCart 0
+java RandomDB Customer 500
+java RandomDB CartDetails 1000
+java RandomDB Cart 500
+java RandomDB EmptyCustomer 0
+java RandomDB EmptyCart 0
 java ConvertTxtToTbl Customer
 java ConvertTxtToTbl CartDetails
 java ConvertTxtToTbl Cart
