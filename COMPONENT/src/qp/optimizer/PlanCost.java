6d6aaba01d3043801daa30d0650b296cee77928d
hai <1503.hai.nguyen@gmail.com>
2021-03-19 18:18:04 +0800

    add inequality join tests

--- COMPONENT/src/qp/optimizer/PlanCost.java
+++ COMPONENT/src/qp/optimizer/PlanCost.java
@@ -156,7 +156,7 @@ public class PlanCost {
                 joincost = leftpages * rightpages;
                 break;
             case JoinType.BLOCKNESTED:
-                joincost = leftpages + Math.round(Math.ceil((double) leftpages / ((double) numbuff - 2))) * rightpages;
+                joincost = leftpages * 1000 + Math.round(Math.ceil((double) leftpages / ((double) numbuff - 2))) * rightpages;
                 break;
             case JoinType.HASHJOIN:
                 // Checking if can partition in one pass
